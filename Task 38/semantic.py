140a06831db99ef74a69283c1a5d481c00e3fb55
Wickendom <lwick1994@hotmail.com>
2023-02-16 21:34:34 +0000

    Task 38 added

---
+++ Task 38/semantic.py
@@ -0,0 +1,32 @@
+import spacy
+nlp = spacy.load('en_core_web_md')
+word1 = nlp("cat")
+word2 = nlp("monkey")
+word3 = nlp("banana")
+print(word1.similarity(word2))
+print(word3.similarity(word2))
+print(word3.similarity(word1))
+
+# it is interesting to see that a cat and monkey are similar as they are both animals, but monkeys and bananas are
+# similar because monkeys are related with bananas. this is proven as bananas and cats are not similar
+
+tokens = nlp('cat apple monkey banana ')
+for token1 in tokens:
+    for token2 in tokens:
+        print(token1.text, token2.text, token1.similarity(token2))
+
+sentence_to_compare = "Why is my cat on the car"
+sentences = ["where did my dog go",
+"Hello, there is my car",
+"I\'ve lost my car in my car",
+"I\'d like my boat back",
+"I will name my dog Diana"]
+model_sentence = nlp(sentence_to_compare)
+for sentence in sentences:
+    similarity = nlp(sentence).similarity(model_sentence)
+    print(sentence + " - ", similarity)
+
+
+# I ran the example file using the simple model and found that the results tended to be a lot less similar.
+# this is likely due to it not being as in depth as the other model and therefor is unable to recognise
+# some similarities
